2b06c5e6b5d38ad370509ed3ccb5ecd0f05212d7
Rob Adams <robert.andrew.adams@gmail.com>
2016-11-08 19:53:00 +0000

    stage 10 - passed test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,10 @@
 require './lib/docking_station.rb'
+require './lib/bike.rb'
 
 describe DockingStation do
   it {is_expected.to respond_to :release_bike}
+  it "shows if bike is working" do
+    bike = subject.release_bike
+    expect(bike.working?).to be
+  end
 end
