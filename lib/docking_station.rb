4789e4f35b3ad5159acb5e4f039ecd6cb2de9bc6
Rob Adams <robert.andrew.adams@gmail.com>
2016-11-09 18:00:32 +0000

    stage 11 completed

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,16 +1,15 @@
 require_relative 'bike'
 
 class DockingStation
+
+  attr_reader :bike
+
   def release_bike
     Bike.new
   end
 
-  def return_bike
-
+  def dock_bike(bike)
+    @bike = bike
   end
 
-  def bike_available?
-
-  end
-  
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,5 @@
 require 'docking_station.rb'
+require 'bike.rb'
 
 describe DockingStation do
   it {is_expected.to respond_to :release_bike}
@@ -7,9 +8,24 @@ describe DockingStation do
     expect(bike).to be_working
   end
   it 'tests for repsonse to "return_bike" method' do
-    expect(subject).to respond_to :return_bike
+    expect(subject).to respond_to :dock_bike
   end
-  it 'test for the bike_available method' do
-    expect(subject).to respond_to :bike_available?
+  # it 'test for the bike_available method' do
+  #   expect(subject).to respond_to :bike_available?
+  # end
+  it 'tests if bike is available?' do
+    bike_ready = subject.release_bike
+    expect(bike_ready).to respond_to :bike_available?
+  end
+  it 'tests if bike is docked' do
+    expect(subject).to respond_to(:dock_bike).with(1).argument
+  end
+  it 'tests for respsonse calling station.bike' do
+    expect(subject).to respond_to :bike
+  end
+  it 'tests for bike dock_bike(bike) and station(class) method "bike" to return bike instance variable' do
+    bike = Bike.new
+    expect(subject.dock_bike(bike)).to eq bike
+    expect(subject.bike).to eq bike
   end
 end
