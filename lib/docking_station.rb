24070824de4fee5da7a3e7f7c6d7c20a308a699b
Rob Adams <robert.andrew.adams@gmail.com>
2016-11-09 12:07:28 +0000

    start of stage 11

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,16 @@
-require_relative "./bike"
+require_relative 'bike'
 
 class DockingStation
   def release_bike
     Bike.new
   end
+
+  def return_bike
+
+  end
+
+  def bike_available?
+
+  end
+  
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,10 +1,15 @@
-require './lib/docking_station.rb'
-require './lib/bike.rb'
+require 'docking_station.rb'
 
 describe DockingStation do
   it {is_expected.to respond_to :release_bike}
   it "shows if bike is working" do
     bike = subject.release_bike
-    expect(bike.working?).to be
+    expect(bike).to be_working
+  end
+  it 'tests for repsonse to "return_bike" method' do
+    expect(subject).to respond_to :return_bike
+  end
+  it 'test for the bike_available method' do
+    expect(subject).to respond_to :bike_available?
   end
 end
